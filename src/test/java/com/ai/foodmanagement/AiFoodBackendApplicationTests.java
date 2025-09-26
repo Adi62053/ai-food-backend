00148a07c844a845b345c562c4585d619c723b0f
Adi62053 <you@example.com>
2025-09-26 08:42:31 +0530

    update2

---
+++ src/test/java/com/ai/foodmanagement/AiFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.foodmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import foodmanagement.AiFoodBackendApplication; // import your main class
+
+@SpringBootTest(classes = AiFoodBackendApplication.class) // explicitly set main config
+class AiFoodBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
